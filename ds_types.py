4d5abfd7d536ff5224374b414945d6ad1f03bf99
Kamil Rezac <kamil.rezac@accolade.com>
2020-12-28 23:56:55 +0100

    optional fields

--- ds_types.py
+++ ds_types.py
@@ -6,27 +6,27 @@ from datetime import datetime
 class CarStatus(BaseModel):
     id: int
     date: datetime
-    latitude: float
-    longitude: float
+    latitude: Optional[float]
+    longitude: Optional[float]
     speed: Optional[float]
-    power: float
-    odometer: float
+    power: Optional[float]
+    odometer: Optional[float]
     ideal_battery_range_km: Optional[float]
-    battery_level: int
+    battery_level: Optional[int]
     outside_temp: Optional[float]
     elevation: Optional[float]
-    fan_status: int
-    driver_temp_setting: float
-    passenger_temp_setting: float
-    is_climate_on: str
-    is_rear_defroster_on: str
-    is_front_defroster_on: str
-    car_id: int
+    fan_status: Optional[int]
+    driver_temp_setting: Optional[float]
+    passenger_temp_setting: Optional[float]
+    is_climate_on: Optional[str]
+    is_rear_defroster_on: Optional[str]
+    is_front_defroster_on: Optional[str]
+    car_id: Optional[int]
     drive_id: Optional[int]
     inside_temp: Optional[float]
-    battery_heater: str
-    battery_heater_on: str
-    battery_heater_no_power: str
+    battery_heater: Optional[str]
+    battery_heater_on: Optional[str]
+    battery_heater_no_power: Optional[str]
     est_battery_range_km: Optional[float]
     rated_battery_range_km: Optional[float]
-    usable_battery_level: int
+    usable_battery_level: Optional[int]
